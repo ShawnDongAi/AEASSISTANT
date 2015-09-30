2bec4599730def80f65fca16acc40b7cdd9df1ea
Shawn <shown.ai@163.com>
2015-09-30 18:11:28 +0800

    update

--- AEAssistant/src/com/zzn/aeassistant/constants/CodeConstants.java
+++ AEAssistant/src/com/zzn/aeassistant/constants/CodeConstants.java
@@ -7,22 +7,22 @@ public class CodeConstants {
 	public static final int REQUEST_CODE_REFRESH = 2;
 	public static final int REQUEST_CODE_VOICE = 3;
 	public static final int REQUEST_CODE_GETFILE = 4;
-	
+
 	public static final String KEY_USER_ID = "user_id";
 	public static final String KEY_USER_PHONE = "user_phone";
 	public static final String KEY_USER_PHONE_EDITABLE = "user_phone_editable";
-	
+
 	public static final String KEY_PROJECT_VO = "project_vo";
 	public static final String KEY_PROJECT_ID = "project_id";
-	
+
 	public static final String KEY_START_DATE = "start_date";
 	public static final String KEY_END_DATE = "end_date";
-	
+
 	public static final String KEY_ATTENDENCE_LIST = "attendance_list";
-	
+
 	public static final String KEY_TOTAL_COUNT = "total_count";
 	public static final String KEY_EXCEPTION_COUNT = "exception_count";
-	
+
 	public static final String KEY_TITLE = "title";
 	public static final String KEY_DEFAULT_TEXT = "default_text";
 	public static final String KEY_HINT_TEXT = "hint_text";
@@ -30,11 +30,14 @@ public class CodeConstants {
 	public static final String KEY_TEXT_RESULT = "text_result";
 	public static final String KEY_INPUT_TYPE = "input_type";
 	public static final String KEY_EDITABLE = "editable";
-	
+
 	public static final String KEY_IDCARD_FRONT = "idcard_front";
 	public static final String KEY_IDCARD_BACK = "idcard_back";
 	public static final String KEY_IDCARD_HAND = "idcard_hand";
-	
+
 	public static final String KEY_POST_ID = "post_id";
 	public static final String KEY_POST_VO = "post_vo";
+
+	public static final String KEY_SCAN_RESULT = "scan_result";
+	public static final String KEY_SCAN_RESULT_FORMAT = "scan_result_format";
 }
