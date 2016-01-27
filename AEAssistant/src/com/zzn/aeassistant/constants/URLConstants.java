ccbecbf47e5ac71a82d2c3fb3bc167ad16d1f8cc
Shawn <shown.ai@163.com>
2016-01-27 17:35:49 +0800

    update

--- AEAssistant/src/com/zzn/aeassistant/constants/URLConstants.java
+++ AEAssistant/src/com/zzn/aeassistant/constants/URLConstants.java
@@ -9,7 +9,8 @@ public class URLConstants {
 	//卧龙晓城
 //	public static final String URL_BASE = "http://192.168.0.100:8080/AENote_Server/";
 	//服务器
-	public static final String URL_BASE = "http://112.124.16.245:8080/AENote_Server/";
+//	public static final String URL_BASE = "http://112.124.16.245:8080/AENote_Server/";
+	public static final String URL_BASE = "http://www.zzn.net.cn/AENote_Server/";
 	//斗西路
 //	public static final String URL_BASE = "http://172.18.117.9:8080/AENote_Server/";
 	//南方科宇
